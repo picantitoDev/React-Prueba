09b48dec66fa78624df293223ce59f25148e1fc5
Piero Arturo <piero.dev@outlook.com>
2025-07-26 23:47:50 -0500

    Passing data between components

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,29 +1,30 @@
-import { recipes } from './data.js';
+function Button(props){
+  const styles = {
+    color: props.color,
+    fontSize : props.fontSize,
+    border: props.border,
 
-function Recipe(props){
+  }
+
   return(
     <>
-      <div key={props.recipe.id}>
-          <h2>{props.recipe.name}</h2>
-          <ul>
-            {props.recipe.ingredients.map(ingredient =>
-              <li key={ingredient}>
-                {ingredient}
-              </li>
-            )}
-          </ul>
-        </div>
+      <button style={styles}>{props.text}</button>
     </>
   )
 }
 
-export default function RecipeList() {
-  return (
-    <div>
-      <h1>Recipes</h1>
-      {recipes.map(recipe =>
-          <Recipe recipe={recipe}></Recipe>
-      )}
-    </div>
-  );
+export default function App(){
+
+    const styles = {
+      text: "Valcrita",
+      color: 'blue',
+      border: '2px solid red',
+      fontSize : '18px'
+  }
+
+  return(
+    <>
+      <Button {...styles}></Button>
+    </>
+  )
 }
