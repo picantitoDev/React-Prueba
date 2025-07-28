67dee7a1cd754a4e1331d7dac21bb4b0604a5f80
Piero Arturo <piero.dev@outlook.com>
2025-07-28 13:41:07 -0500

    children exercise

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,57 +1,16 @@
-import { getImageUrl } from "./utils.js";
-
-function Profile({scientistName, imageID, profession, awards, discovery, imageSize=70}){
-  return(
-    <section className="profile">
-        <h2>{scientistName}</h2>
-        <img
-          className="avatar"
-          src={getImageUrl(imageID)}
-          alt={scientistName}
-          width={imageSize}
-          height={imageSize}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            {profession}
-          </li>
-          <li>
-            <b>Awards: {awards.length} </b> 
-            ({awards.join(',')})
-          </li>
-          <li>
-            <b>Discovered: </b>
-            {discovery}
-          </li>
-        </ul>
-      </section>
-  );
+function Box({children, styles}){
+  return (
+    <div style={styles}>{children}</div>
+  )
 }
 
-export default function Gallery() {
-  const scientists = [
-    {
-      scientistName: "Katsuko Saruhashi",
-      imageID: "YfeOqp2",
-      profession: "geochemist",
-      awards: ["Miyake Prize for geochemistry", "Tanaka Prize"],
-      discovery: "a method for measuring carbon dioxide in seawater"  
-    },
-    {
-      // Puedes agregar otro científico aquí si lo deseas
-      scientistName: "Marie Curie",
-      imageID: "yBXx9ar",
-      profession: "physicist and chemist",
-      awards: ["Nobel Prize in Physics", "Nobel Prize in Chemistry"],
-      discovery: "radioactivity"
-    }
-  ];
-
-  return (
-    <div>
-      <h1>Notable Scientists</h1>
-        <Profile {...scientists[0]}></Profile>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+    <Box styles={{border: '2px solid red', padding: '1rem', fontSize: '24px', textAlign: 'center'}}>
+        <p>Todo se va a la gran pucta</p>
+        <small>onioooo</small>
+    </Box>
+    </>
+  )
 }
