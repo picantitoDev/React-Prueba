1163725f9661e06c9003cfaa67c075fc5af3fea2
Piero Arturo <piero.dev@outlook.com>
2025-07-31 23:46:48 -0500

    A bit of state manipulation

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, {useState} from "react"
+import './App.css'
 
-const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+export default function App(){
+  const COLORES = ['red', 'green', 'blue']
 
-export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [ backgroundColor, setBackgroundColor ] = useState(COLORES[0])  
+  const [ borderColor, setBorderColor ] = useState(COLORES[0])
 
-  const onButtonClick = (color) => () => {
+  const changeColor = (color) => {
     setBackgroundColor(color)
   }
 
+  const changeBorderColor = (color) => {
+    setBorderColor(color)
+  }
+
+  const display = (color) => {
+    changeColor(color)
+    changeBorderColor(color)
+  }
+
+  const currentIndex = COLORES.indexOf(backgroundColor)
   return (
-    <div className="App" style={{ backgroundColor: backgroundColor, }}>
-      {COLORS.map((color) => (
-        <button type="button" key={color} onClick={onButtonClick(color)} className={backgroundColor === color ? 'selected' : ''}>
-          {color}
-        </button>
-      ))}
+    <div className="App" style={{backgroundColor}}>
+      <h1>Colores!</h1>
+      {COLORES.map((color) =>{
+        return(
+          <button onClick={() => display(color)} key={color} className={backgroundColor === color ? 'selected' : ''} style={{outline: `5px solid ${currentIndex+1 === COLORES.length ? COLORES[currentIndex- 1]: COLORES[0]}`,}}>{color}</button>
+        )
+      })}
     </div>
   )
 }
